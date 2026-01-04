b93bc92fac3efc422ae2c74ea3e491c8a963a656
Ozodbek <ozodbek200017@gmail.com>
2026-01-04 15:43:33 +0600

    dsfdgf7

--- src/pages/RecoverAccount/RecoverAccount.jsx
+++ src/pages/RecoverAccount/RecoverAccount.jsx
@@ -1,5 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./Aboutmysel.module.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function RecoverAccount() {
-  return <div>RecoverAccount</div>;
+  const [email, setEmail] = useState("");
+  const [token, setToken] = useState("");
+  const [df2, setDf2] = useState(false);
+  const [canSend, setCanSend] = useState(true);
+  const [canSend2, setCanSend2] = useState(true);
+
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    if (!email) return alert("Введите email!");
+    if (!canSend) return alert("Подождите 20 секунд перед повторной отправкой");
+
+    setCanSend(false);
+    setTimeout(() => setCanSend(true), 50000);
+    fetch("http://localhost:7000/recover-account", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ emailInput: email }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.message);
+        setDf2(data.success);
+        setEmail("");
+      })
+      .catch((error) => console.error(error));
+  }
+
+  function handleTokenCheck() {
+    if (!token) return alert("Введите token!");
+    if (!canSend2)
+      return alert("Подождите 20 секунд перед повторной отправкой");
+
+    fetch("http://localhost:7000/time-check", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ tokenInput: token.trim() }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data.message);
+        alert(
+          `вот ваш логин и пароль username:${data.username} и Password:${data.Password}`
+        );
+        setToken("");
+        setCanSend2(false);
+        setTimeout(() => setCanSend2(true), 50000);
+      })
+      .catch((error) => console.error(error));
+  }
+
+  return (
+    <div className={styles.RecoverAccount}>
+      <div className={styles.card}>
+        <h2 className={styles.title}>Восстановление аккаунта</h2>
+
+        <p className={styles.description}>
+          Введите email, указанный при регистрации. Мы отправим инструкции по
+          восстановлению доступа.(Пожалуйста, подождите 30 секунд после
+          отправки.)
+        </p>
+
+        <div className={styles.field}>
+          <label>Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="myemail@mail.com"
+          />
+          <span className={styles.hint}>
+            Проверьте папку «Спам», если письмо не пришло
+          </span>
+        </div>
+
+        {df2 && (
+          <div className={styles.field}>
+            <label>Введите токен</label>
+            <input
+              value={token}
+              onChange={(e) => setToken(e.target.value)}
+              type="text"
+              placeholder="Введите код из письма"
+            />
+            <button onClick={handleTokenCheck} className={styles.submit}>
+              проверить токен
+            </button>
+          </div>
+        )}
+
+        <button onClick={handleSubmit} className={styles.submit}>
+          Отправить письмо
+        </button>
+
+        <button onClick={() => navigate("/")} className={styles.back}>
+          ← Вернуться ко входу
+        </button>
+      </div>
+    </div>
+  );
 }
