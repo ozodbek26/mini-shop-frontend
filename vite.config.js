ae07d8b5c275e3a2bd5b4b418666daf9e3b3e49f
Ozodbek <ozodbek200017@gmail.com>
2026-01-06 21:24:29 +0600

    fix custom domain config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/mini-shop-frontend/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mini-shop-frontend/",
+  base: "/", // 👈 ВАЖНО
 });
