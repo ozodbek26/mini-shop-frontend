22bbfaed3ec3af2bdceb10861b283b3df9e2ac65
Ozodbek <ozodbek200017@gmail.com>
2026-01-06 12:56:03 +0600

    dsfdgf6

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 import App from "./App";
 // import "./i18n/i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
